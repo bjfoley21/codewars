55bd2edbaaa9692985ab7404b8a69d30358974be
bjfoley21 <bjfoley21@gmail.com>
2023-01-31 21:32:08 -0500

    8

---
+++ 8kyu/mangos.js
@@ -0,0 +1,7 @@
+//DESCRIPTION:
+//There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+function mango(quantity, price){
+    let freeMangos = Math.floor(quantity / 3);
+  return (quantity - freeMangos) * price;
+  }
