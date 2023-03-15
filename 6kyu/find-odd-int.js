74ef257cfad1fe56417f19a87f336712bbc828ae
bjfoley21 <bjfoley21@gmail.com>
2023-03-14 23:29:12 -0400

    6kyu

---
+++ 6kyu/find-odd-int.js
@@ -0,0 +1,10 @@
+function findOdd(A) {
+  var obj = {};
+  A.forEach(function(el){
+    obj[el] ? obj[el]++ : obj[el] = 1;
+  });
+  
+  for(prop in obj) {
+    if(obj[prop] % 2 !== 0) return Number(prop);
+  }
+}
